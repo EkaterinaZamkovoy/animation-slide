2ac9c9db1facd50c09c7f765c79a34f3cdec8b5d
EkaterinaZamkovoy <ekaterinazamkovoy@yandex.ru>
2025-07-10 23:39:13 +0300

    feat: add animation for molecules

--- js/main.js
+++ js/main.js
@@ -22,3 +22,64 @@ function scaleSlide() {
 
 window.addEventListener('load', scaleSlide);
 window.addEventListener('resize', scaleSlide);
+
+// Анимация молекул
+const molecules = document.querySelectorAll(
+  '.microelements, .probiotics, .protein'
+);
+const orbit = document.getElementById('orbit');
+const centerX = orbit.offsetWidth / 2 + 90;
+const centerY = orbit.offsetHeight / 2 + 50;
+
+// радиусы
+const rx = 320;
+const ry = 130;
+
+const minSize = 145;
+const midSize = 195;
+const maxSize = 330;
+
+let angle = 0;
+
+function animate() {
+  molecules.forEach((molecule, index) => {
+    const offset = (index * (Math.PI * 2)) / molecules.length; // равномерно распределить
+    const currentAngle = angle + offset;
+    const x = centerX + rx * Math.cos(currentAngle);
+    const y = centerY + ry * Math.sin(currentAngle);
+
+    // Угол в диапазоне [0, 2π]
+    const normalizedAngle =
+      ((currentAngle % (2 * Math.PI)) + 2 * Math.PI) % (2 * Math.PI);
+
+    const angleDeg = (normalizedAngle * 180) / Math.PI;
+
+    let size = midSize;
+    let zIndex = 1;
+
+    if (angleDeg >= 200 && angleDeg <= 340) {
+      //   size = minSize;
+      zIndex = 0;
+    } else if (
+      (angleDeg >= 80 && angleDeg <= 100) ||
+      (angleDeg >= 260 && angleDeg <= 280)
+    ) {
+      //   size = midSize;
+      zIndex = 1;
+    } else if (angleDeg >= 20 && angleDeg <= 160) {
+      //   size = maxSize;
+      zIndex = 2;
+    }
+
+    molecule.style.position = 'absolute';
+    molecule.style.left = `${x}px`;
+    molecule.style.top = `${y}px`;
+    molecule.style.maxWidth = `${size}px`;
+    molecule.style.zIndex = zIndex;
+  });
+
+  angle += 0.01; // скорость вращения
+  requestAnimationFrame(animate);
+}
+
+animate();
